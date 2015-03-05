d70922101c01c7e4e9dde099bba99a3e31dc22f6
sorhent <sorhent@users.noreply.github.com>
2015-03-05 21:13:01 +0100

    A little modif

--- we/src/main/java/org/home/we/App.java
+++ we/src/main/java/org/home/we/App.java
@@ -18,6 +18,7 @@ public class App {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World!");
+		System.out.println("Hello world 2");
 
 		LOGGER.debug("Hello World! LOGGER");
 
